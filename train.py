a5abbd8e9f07b95b29bb4d0dea995769d8121587
DanielKalicki <danielkalicki@gmail.com>
2024-01-17 20:35:44 +0100

    PPO model working on Pong, Boxing, Enduro atari games

--- train.py
+++ train.py
@@ -8,10 +8,10 @@ from torch import nn
 
 from torchrl.collectors import SyncDataCollector
 from torchrl.data.replay_buffers import TensorDictReplayBuffer, \
-    LazyTensorStorage, SamplerWithoutReplacement
+    LazyTensorStorage, SamplerWithoutReplacement, TensorDictPrioritizedReplayBuffer
 from torchrl.envs.libs.gym import GymEnv
 from torchrl.modules import ProbabilisticActor, ValueOperator, TanhNormal
-from torchrl.objectives import ClipPPOLoss
+from torchrl.objectives import ClipPPOLoss, A2CLoss
 from torchrl.objectives.value import GAE
 
 from torchrl.envs import (
@@ -41,6 +41,7 @@ from torchrl.modules import (
     ProbabilisticActor,
     TanhNormal,
     ValueOperator,
+    NoisyLinear,
 )
 from collections import OrderedDict, defaultdict
 from torchrl.data.replay_buffers import ReplayBuffer
@@ -57,20 +58,22 @@ from tensordict import TensorDict
 import time
 import math
 import random
+import torch.nn.functional as F
 
-# from utils_atari import eval_model, make_parallel_env, make_ppo_models
-
-# if args.log:
-#     import wandb
-#     wandb.init(project='rl_atari_gato', entity='danielkalicki')
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
 device = "cpu" if not torch.cuda.device_count() else "cuda"
-# device = "cpu"
+
+class CReLU(nn.Module):
+    def __init__(self, inplace=False):
+        super(CReLU, self).__init__()
+
+    def forward(self, x):
+        x = torch.cat((x,-x),dim=-1)
+        return F.relu(x)
 
 def make_env(env_name, num_envs, test=False):
     def make_base_env(env_name, test=False):
-        if not test:
-            print("\n\n\n\n\n\n", env_name)
         env = GymEnv(env_name, frame_skip=4, from_pixels=True, pixels_only=False, device=device, full_action_space=True)
         env = TransformedEnv(env)
         env.append_transform(NoopResetEnv(noops=30, random=True))
@@ -80,66 +83,6 @@ def make_env(env_name, num_envs, test=False):
 
     if num_envs > 1:
         env = ParallelEnv(num_envs, EnvCreator(lambda: make_base_env(env_name, test)))
-        # env = ParallelEnv(30, [
-        #     # EnvCreator(lambda: make_base_env("AtlantisNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("FishingDerbyNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("EnduroNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("DemonAttackNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("GopherNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("DoubleDunkNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("FreewayNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("TennisNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     EnvCreator(lambda: make_base_env("BoxingNoFrameskip-v4")),
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        #     # EnvCreator(lambda: make_base_env("PongNoFrameskip-v4")), 
-        # ])
-        # print(env)
     else:
         env = make_base_env(env_name, test)
     env = TransformedEnv(env)
@@ -160,15 +103,21 @@ def main():
     # env_name = "PongNoFrameskip-v4"
     # env_name = "FreewayNoFrameskip-v4"
     env_name = "EnduroNoFrameskip-v4"
-    env_name = "DoubleDunkNoFrameskip-v4"
+    # env_name = "DoubleDunkNoFrameskip-v4"
+    # env_name = "GopherNoFrameskip-v4"
+    # env_name = "FishingDerbyNoFrameskip-v4"
+    # env_name = "TennisNoFrameskip-v4"
+    # env_name = "QbertNoFrameskip-v4"
     frame_skip = 4
     total_frames = 40_000_000 // frame_skip
-    frames_per_batch = 4096 // frame_skip
+    frames_per_batch = 4096*4 // frame_skip
+    storage_frames_per_batch = frames_per_batch
     num_epochs = 10
-    sub_batch_size = 1024
-    max_grad_norm = 0.5
+    sub_batch_size = 1024*1
+    max_grad_norm = 40
 
-    tr_input_dim = 512
+    # tr_input_dim = 2048
+    tr_input_dim = 64*7*7
 
     if args.log:
         exp_name = generate_exp_name("PPO", f"Atari_{env_name}")
@@ -176,16 +125,19 @@ def main():
             "wandb", logger_name="ppo", experiment_name=exp_name, project='rl_atari_gato', entity='danielkalicki'
         )
 
-    test_env_pong = make_env("PongNoFrameskip-v4", 1, test=True)
-    test_env_boxing = make_env("BoxingNoFrameskip-v4", 1, test=True)
-    test_env_freeway = make_env("FreewayNoFrameskip-v4", 1, test=True)
-    test_env_freeway = make_env("EnduroNoFrameskip-v4", 1, test=True)
-    test_env_freeway = make_env("DoubleDunkNoFrameskip-v4", 1, test=True)
-    test_envs = [test_env_pong, test_env_boxing, test_env_freeway]
+    # test_env_pong = make_env("PongNoFrameskip-v4", 1, test=True)
+    # test_env_boxing = make_env("BoxingNoFrameskip-v4", 1, test=True)
+    # test_env_freeway = make_env("FreewayNoFrameskip-v4", 1, test=True)
+    test_env_enduro = make_env("EnduroNoFrameskip-v4", 1, test=True)
+    # test_env_doubledunk = make_env("DoubleDunkNoFrameskip-v4", 1, test=True)
+    # test_env_gopher = make_env("GopherNoFrameskip-v4", 1, test=True)
+    # test_env_fishing = make_env("FishingDerbyNoFrameskip-v4", 1, test=True)
+    # test_env_tennis = make_env("TennisNoFrameskip-v4", 1, test=True)
+    # test_env_qbert = make_env("QbertNoFrameskip-v4", 1, test=True)
+    test_envs = [test_env_enduro]
     test_env = test_envs[-1]
 
     action_dim = test_env.action_spec.shape[0]
-    # env = GymEnv(env_name, frame_skip=4, from_pixels=True, pixels_only=False, device=device,)
 
     # ------------------ TRANSFORMER ------------------
     class PositionalEncoding(nn.Module):
@@ -213,38 +165,17 @@ def main():
             self.state_enc_net = nn.Sequential(OrderedDict([
                 ('ConvNet', ConvNet(
                     activation_class=torch.nn.ReLU,
-                    num_cells=[32, 64, 64],
+                    num_cells=[32*2, 64*4, 64],
                     kernel_sizes=[8, 4, 3],
                     strides=[4, 2, 1],
                 )),
-                ('fc1',             nn.Linear(64*7*7, tr_input_dim)), # [B, 512]
-                ('act1',            nn.ReLU()),
             ])).to(device)
-
-            self.action_enc_net = nn.Sequential(OrderedDict([
-                ('emb', nn.Embedding(18, tr_input_dim)), # long???? TODO wrong
-                ('act1',            nn.ReLU()),
-            ])).to(device)
-
-            # self.pos_encoder = PositionalEncoding(tr_input_dim, dropout=0.0, max_len=512)
-            # encoder_layer = nn.TransformerEncoderLayer(d_model=tr_input_dim, nhead=tr_input_dim//16, activation="gelu", norm_first=True, dropout=0.0)
-            # self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=4)
 
         def forward(self, x_1, x_2):
             if x_2 is None: # first action
-                # print("-------")
                 x_2 = torch.zeros((x_1.shape[0], self.action_dim), dtype=torch.int64).to(device)
 
-            # print("pixels.shape", x_1.shape)
-            # print("action.shape", x_2.shape)
-
             state_enc = self.state_enc_net(x_1) 
-
-            x_2 = torch.argmax(x_2, dim=-1)
-            action_enc = self.action_enc_net(x_2)
-
-            # print("state_enc.shape", state_enc.shape)
-            # print("action_enc.shape", action_enc.shape)
 
             return state_enc
 
@@ -254,7 +185,6 @@ def main():
         module = transformer_net,
         out_keys=["tr_out"]
     )
-
 
     # ------------------ ACTOR ------------------
     actor_net = nn.Sequential(OrderedDict([
@@ -287,6 +217,7 @@ def main():
         ('fc1',     nn.Linear(tr_input_dim, 512)),
         ('act1',    nn.ReLU()),
         ('fc2',     nn.Linear(512, 1)), # [B, ValueDim=1]
+        ('act2',    nn.Tanh()),
     ])).to(device)
     value_module = TensorDictModule(
         in_keys = ["tr_out"],
@@ -297,8 +228,7 @@ def main():
 
     # ------------------ DATA COLLECTION ------------------
     collector = SyncDataCollector(
-        # create_env_fn=make_env(env_name, 16),
-        create_env_fn=make_env(env_name, 4),
+        create_env_fn=make_env(env_name, 8),
         policy=policy_module,
         frames_per_batch=frames_per_batch,
         total_frames=total_frames,
@@ -309,39 +239,39 @@ def main():
 
     sampler = SamplerWithoutReplacement()
     replay_buffer = TensorDictReplayBuffer(
-        storage=LazyMemmapStorage(frames_per_batch),
+        storage=LazyMemmapStorage(storage_frames_per_batch, device="cpu"),
         sampler=sampler,
         batch_size=sub_batch_size,
     )
 
     # ------------------ LOSS ------------------
     advantage_module = GAE(
-        gamma=0.99, lmbda=0.95, value_network=value_module, average_gae=False
+        gamma=0.9, lmbda=0.95, value_network=value_module, average_gae=False
     )
-
     loss_module = ClipPPOLoss(
         actor=policy_module,
         critic=value_module,
-        clip_epsilon=0.1, # 0.1
+        clip_epsilon=0.1,
         entropy_bonus=bool(1e-4),
-        entropy_coef=0.01, #1e-4,
+        entropy_coef=0.01, #1e-3,
         critic_coef=1.0,
-        # gamma=0.99,
-        loss_critic_type="smooth_l1",
-        normalize_advantage=False,
+        separate_losses=False, # loss function is decreasing
+        loss_critic_type="l2", # smooth_l1 - >based on https://arxiv.org/pdf/2011.14826v2.pdf hubbert loss is better than L2
+        normalize_advantage=True, # improves performance on 2 tasks
     )
+
     advantage_module.set_keys(done="end-of-life", terminated="end-of-life")
     loss_module.set_keys(done="end-of-life", terminated="end-of-life")
 
     # ------------------ OPTIMIZER ------------------
-    optim = torch.optim.Adam(loss_module.parameters(), lr=5e-5, weight_decay=0.0, eps=1e-6)
+    optim = torch.optim.Adam(loss_module.parameters(), lr=2.5e-4, weight_decay=0.0, eps=1e-6) # weight_decay=L2=5e-4
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, total_frames // frames_per_batch, 0.0)
 
     # ------------------ TRAIN LOOP ------------------
     collected_frames = 0
     sampling_start = time.time()
     pbar = tqdm(total=total_frames)
-    losses = TensorDict({}, batch_size=[num_epochs, frames_per_batch // sub_batch_size])
+    losses = TensorDict({}, batch_size=[num_epochs, storage_frames_per_batch // sub_batch_size])
 
     for i, data in enumerate(collector):
         log_info = {}
@@ -364,7 +294,6 @@ def main():
             # Compute GAE
             with torch.no_grad():
                 data = advantage_module(data)
-            # print(data)
             data_reshape = data.reshape(-1)
 
             # Update the replay buffer
@@ -376,7 +305,6 @@ def main():
 
                 # Forward pass PPO loss
                 loss = loss_module(batch)
-                # print(loss)
                 losses[j, k] = loss.select("loss_critic", "loss_entropy", "loss_objective").detach()
                 loss_sum = (loss["loss_critic"] + loss["loss_objective"] + loss["loss_entropy"])
 
@@ -399,17 +327,15 @@ def main():
             "train/training_time": training_time,
         })
 
-        # print("------")
-
         # Get test rewards
         with torch.no_grad(), set_exploration_type(ExplorationType.MODE):
-            for test_env in [test_envs[-1]]:
+            # for test_env in [test_envs[-1]]:
+            for test_env in test_envs:
                 policy_module.eval()
                 eval_start = time.time()
-                # test_rewards = eval_model(actor, test_env, num_episodes=10)
                 test_rewards = []
                 for _ in range(1):
-                    td_test = test_env.rollout(policy=policy_module, auto_reset=True, auto_cast_to_device=True, break_when_any_done= True, max_steps=10_000_000)
+                    td_test = test_env.rollout(policy=policy_module, auto_reset=True, auto_cast_to_device=True, break_when_any_done= True, max_steps=10_000)
                     reward = td_test["next", "episode_reward"][td_test["next", "done"]]
                     test_rewards.append(reward.cpu())
                 del td_test
@@ -419,10 +345,17 @@ def main():
                 eval_time = time.time() - eval_start
                 log_info.update({f"eval/{test_env.env_name}/reward": test_rewards.mean(),f"eval/{test_env.env_name}/time": eval_time,})
                 
-                
         if args.log:
             for key, value in log_info.items():
                 logger.log_scalar(key, value, collected_frames)
+
+        if i%10 == 0:
+            save_dict = {
+                'epoch': i,
+                'policy_module': policy_module.state_dict(),
+                'value_module': value_module.state_dict()
+            }
+            torch.save(save_dict, f"./save/{env_name}_{timestr}")
 
         collector.update_policy_weights_()
         scheduler.step()
